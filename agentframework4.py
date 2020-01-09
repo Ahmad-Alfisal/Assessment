d3ed4012fe3076b9041969e94e7b8858dbb97f77
Ahmad-Alfisal <58438509+Ahmad-Alfisal@users.noreply.github.com>
2020-01-09 01:43:28 +0000

    Update agentframework4.py

--- agentframework4.py
+++ agentframework4.py
@@ -21,21 +21,16 @@ class Agent:
         return "x=" + str(self.x) + ",y=" + str(self.y) + "store" + str(self.store)  
     
     def move(self):
-        #print("move")
-        #print("from ", self.x, self.y)
         if random.random() < 0.5:
             self.x = (self.x + 1) % len(self.enviroment)
-            #print("+ x")
         else:
             self.x = (self.x - 1) % len(self.enviroment)
-            #print("- x")
+    
         if random.random() < 0.5:
             self.y = (self.y + 1) % len(self.enviroment)
-            #print("+ y")
         else:
             self.y = (self.y - 1) % len(self.enviroment)
-            #print("- y")
-        #print("to ", self.x, self.y)
+
     def eat(self):
             if self.enviroment[self.y][self.x] >10:
                 self.enviroment[self.y][self.x]-= 10
@@ -43,9 +38,7 @@ class Agent:
             else:
                 self.store +=   self.enviroment[self.y][self.x]
                 self.enviroment[self.y][self.x]=0
-#            if self.store > 100:
-#                 self.enviroment[self.y][self.x]+= self.store
-#                 self.store = 0
+
               
     def share_with_neighbours(self, neighbourhood):
         for agent in self.agents:
@@ -54,7 +47,7 @@ class Agent:
                 average_store = (self.store + agent.store)/ 2
                 self.store = average_store
                 agent.store = average_store
-#                print("sharing " + str(distance) + " " + str(average_store))
+
                 
     def distance_between(self, agent):
         return ((self.y- agent.y)**2 +(self.x - agent.x)**2)**0.5
